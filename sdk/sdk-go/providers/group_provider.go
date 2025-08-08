ca73136229aff8cfa500c5c95f89753f0cd68a91
Flo <144521337+FlorianLang06@users.noreply.github.com>
2025-08-08 19:43:05 +0200

    fix: first group provider method

---
+++ sdk/sdk-go/providers/group_provider.go
@@ -0,0 +1,25 @@
+package providers;
+
+import (
+	"context"
+
+	pb "polocloud/sdk/gen/polocloud/v1/proto"
+	grpc "google.golang.org/grpc"
+)
+
+type GroupProvider struct {
+	client	pb.GroupControllerClient
+}
+
+func NewGroupProvider(conn *grpc.ClientConn) GroupProvider {
+	return GroupProvider{client: pb.NewGroupControllerClient(conn)}
+}
+
+func (provider GroupProvider) FindAll() ([]*pb.GroupSnapshot, error) {
+	result, err := provider.client.Find(context.Background(), &pb.FindGroupRequest{})
+	if err != nil {
+		return nil, err
+	}
+
+	return result.Groups, nil
+}
--- sdk/sdk-go/sdk.go
+++ sdk/sdk-go/sdk.go
@@ -1,1 +1,21 @@
 package sdk
+
+import (
+	grpc "google.golang.org/grpc"
+	providers "polocloud/sdk/providers"
+)
+
+type Polocloud struct {
+	conn			*grpc.ClientConn
+	groupProvider	providers.GroupProvider
+}
+
+func CreatePolocloudClient() (Polocloud, error) {
+	var opts []grpc.DialOption
+	conn, err := grpc.NewClient("http://127.0.0.1:8932", opts...)
+
+	groupProvider := providers.NewGroupProvider(conn)
+
+	return Polocloud { conn, groupProvider }, err
+}
+
