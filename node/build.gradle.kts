f2fda22d430b69420378ab8cd5aaf35ed96cf2fd
httpmarco <httpmarco@gmx.de>
2026-02-08 21:21:24 +0100

    feat: add logging support with Log4j and update Node configuration

--- node/build.gradle.kts
+++ node/build.gradle.kts
@@ -10,7 +10,9 @@ polocloud {
 dependencies {
     compileOnly(projects.common)
     compileOnly(libs.bundles.grpc)
+    compileOnly(libs.bundles.logging)
 
     runtimeOnly(libs.bundles.grpc)
     runtimeOnly(projects.common)
+    runtimeOnly(libs.bundles.logging)
 }
--- node/src/main/kotlin/dev/httpmarco/polocloud/node/Node.kt
+++ node/src/main/kotlin/dev/httpmarco/polocloud/node/Node.kt
@@ -1,11 +1,13 @@
 package dev.httpmarco.polocloud.node
 
 import dev.httpmarco.polocloud.common.grpc.GrpcEndpoint
+import org.slf4j.LoggerFactory
 
 object Node {
+    private val logger = LoggerFactory.getLogger(Node::class.java)
+    private val endpoint = GrpcEndpoint(5467)
 
-    private val endpoint = GrpcEndpoint(8080)
-
-
-
+    init {
+        logger.info("Starting Node")
+    }
 }
