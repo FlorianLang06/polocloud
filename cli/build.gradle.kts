ddef26f8e709704801472494d1c87d642e28d349
RECHERGG <rechergg@gmail.com>
2026-03-07 13:04:35 +0100

    use polocloudRuntime again in cli

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -11,15 +11,14 @@ polocloud {
 }
 
 dependencies {
-    // i dont know why polocloudRuntime is not working here, so i have to add the dependencies manually
-    compileOnly("org.jline:jline:4.0.0")
-    compileOnly(libs.slf4j.api)
-    compileOnly(libs.log4j.api)
-    compileOnly(libs.log4j.core)
-    compileOnly(libs.log4j.slf4j)
+    polocloudRuntime("org.jline:jline:4.0.0")
+    polocloudRuntime(libs.slf4j.api)
+    polocloudRuntime(libs.log4j.api)
+    polocloudRuntime(libs.log4j.core)
+    polocloudRuntime(libs.log4j.slf4j)
 
-    compileOnly(libs.polocloud.i18n)
-    compileOnly(libs.kotlinx.serialization.json)
+    polocloudRuntime(libs.polocloud.i18n)
+    polocloudRuntime(libs.kotlinx.serialization.json)
 
     compileOnly(projects.common)
 }
