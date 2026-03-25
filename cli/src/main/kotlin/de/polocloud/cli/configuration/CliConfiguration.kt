9fff65f1c45813780aa63a5806a91dec666c8500
RECHERGG <rechergg@gmail.com>
2026-03-25 23:13:47 +0100

    impl(config): added new config system into cli and made sure fileWatcher works

--- cli/src/main/kotlin/de/polocloud/cli/configuration/CliConfiguration.kt
+++ cli/src/main/kotlin/de/polocloud/cli/configuration/CliConfiguration.kt
@@ -1,12 +1,22 @@
 package de.polocloud.cli.configuration
 
 import de.polocloud.cli.configuration.serialization.LocaleSerializer
+import de.polocloud.common.configuration.ConfigFile
+import de.polocloud.common.configuration.DefaultableConfig
 import de.polocloud.i18n.model.Language
 import kotlinx.serialization.Serializable
 import java.util.Locale
 
 @Serializable
+@ConfigFile("polocloud-cli.json")
 data class CliConfiguration(
     @Serializable(with = LocaleSerializer::class)
     val locale: Locale = Language.of("en_US")
-)
+) {
+    companion object : DefaultableConfig<CliConfiguration> {
+        override fun createDefault(): CliConfiguration {
+            return CliConfiguration()
+        }
+
+    }
+}
