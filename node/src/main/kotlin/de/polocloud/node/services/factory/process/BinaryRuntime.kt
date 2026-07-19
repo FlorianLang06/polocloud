60c653d100eca1230789770f45f324232753921c
FlorianLang06 <144521337+FlorianLang06@users.noreply.github.com>
2026-07-19 22:48:15 +0200

    feat: set owner execute posix permission if needed

--- node/src/main/kotlin/de/polocloud/node/services/factory/process/BinaryRuntime.kt
+++ node/src/main/kotlin/de/polocloud/node/services/factory/process/BinaryRuntime.kt
@@ -1,6 +1,9 @@
 package de.polocloud.node.services.factory.process
 
 import java.io.File
+import java.nio.file.FileSystems
+import java.nio.file.Files
+import java.nio.file.attribute.PosixFilePermission
 
 object BinaryRuntime : PlatformRuntime {
 
@@ -12,7 +15,19 @@ object BinaryRuntime : PlatformRuntime {
         artifact: File,
         args: List<String>
     ): List<String> {
+        if (isPosixCompatible()) {
+            val permissions = Files.getPosixFilePermissions(artifact.toPath())
+            if (!permissions.contains(PosixFilePermission.OWNER_EXECUTE)) {
+                permissions.add(PosixFilePermission.OWNER_EXECUTE)
+                Files.setPosixFilePermissions(artifact.toPath(), permissions)
+            }
+        }
         return listOf(artifact.absolutePath) + args
+    }
+
+    fun isPosixCompatible(): Boolean {
+        val supportedViews = FileSystems.getDefault().supportedFileAttributeViews()
+        return supportedViews.contains("posix")
     }
 
 }
